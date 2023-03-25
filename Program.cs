506d684ddd0e40003aa504369a9e3029daf6f8c0
LoVladya <128499250+LoVladya@users.noreply.github.com>
2023-03-25 21:04:59 +0200

    Зміни в коді
    Виправлено:
    -Додано наслідування;
    -Виправленно public на private;
    -Додано класс Coordinates, що містить координати.

--- Program.cs
+++ Program.cs
@@ -1,31 +1,42 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Object_oriented_programming_project
 {
     internal class Program
     {
-        class Player
+        class Player:Coordinates
         {
-            public string name { get; set; }
-            public int x { get; set; }
-            public int y { get; set; }
-            public int weaponLevel { get; set; } = 0;
+            private string name { get; set; }
+            private int weaponLevel { get; set; } = 0;
 
-            public Player(string Name, int X, int Y, int WeaponLevel)
+            public Player(string Name, int X, int Y, int WeaponLevel):base(6,3)
             {
                 this.name = Name;
-                this.x = X;
-                this.y = Y;
                 this.weaponLevel = WeaponLevel;
             }
+
         }
-        class Items
+
+        class Coordinates
         {
-            public bool itemUpgrade;
-            public bool armor;
-            public int traps;
-            public Items(bool itemUpgrade, bool armor, int traps)
+            private int x;
+            private int y;
+            public Coordinates(int x, int y)
             {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        class Items:Coordinates
+        {
+
+            private bool itemUpgrade;
+            private bool armor;
+            private int traps;
+            public Items(bool itemUpgrade, bool armor, int traps):base(1,1) 
+            {
+
                 this.itemUpgrade = itemUpgrade;
                 this.armor = armor;
                 this.traps = traps;
@@ -44,8 +55,7 @@ namespace Object_oriented_programming_project
             Player getPlayer = new Player("Петя", 2, 5, 0);
             Items getItems = new Items(false,false,0);
             Map[,] getMap = new Map[10,10];
-
-            Console.WriteLine();
+            Console.WriteLine("sss");
         }
     }
 }
